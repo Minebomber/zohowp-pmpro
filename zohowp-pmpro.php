c3b9aaec27276261570e43ff85a9feb3aa5a51f2
Minebomber <markmc909@gmail.com>
2022-11-05 22:01:07 -0700

    fix naming

--- zohowp-pmpro.php
+++ zohowp-pmpro.php
@@ -10,5 +10,5 @@ Text Domain: zohowp-pmpro
 define('ZOHOWP_PMPRO_DIR_PATH', plugin_dir_path(__FILE__));
 define('ZOHOWP_PMPRO_VERSION', '1.0.0');
 
-require_once ZOHOWP_ELEMENTOR_DIR_PATH . '/includes/class-plugin.php';
-\ZohoWP\Elementor\Plugin::instance();
+require_once ZOHOWP_PMPRO_DIR_PATH . '/includes/class-plugin.php';
+\ZohoWP\PMPro\Plugin::instance();
